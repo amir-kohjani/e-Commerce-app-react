13c1e5cda1a4528839e767c2875efc70d6dcc37d
Amir Kohjani <amir.kohjani@outlook.com>
2022-09-30 13:49:03 +0330

    fixed some Bugs

--- src/components/CustomRadioButton/CustomRadioBtnContainer.jsx
+++ src/components/CustomRadioButton/CustomRadioBtnContainer.jsx
@@ -1,29 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
-import CustomRadioBtn from './CustomRadioBtn'
-
+import React from "react";
+import styled from "styled-components";
+import CustomRadioBtn from "./CustomRadioBtn";
 
 const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-`
-
-const CustomRadioBtnContainer = ({colors , selectedColor}) => {
-    console.log(colors)
+const CustomRadioBtnContainer = ({ colors, selectedColor }) => {
   return (
     <Container>
-
-        {colors&& colors.map((color)=>{return(
-            <CustomRadioBtn color={color} selectedColor={selectedColor} />
-        )})}
-            
-            
-
+      {colors &&
+        colors.map((color) => {
+          return <CustomRadioBtn color={color} selectedColor={selectedColor} />;
+        })}
     </Container>
-  )
-}
+  );
+};
 
-export default CustomRadioBtnContainer
+export default CustomRadioBtnContainer;
