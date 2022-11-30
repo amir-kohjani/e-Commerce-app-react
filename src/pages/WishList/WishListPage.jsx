fab092fbcad8f8f93c7e5ed3b6fe854edbebfb0c
amir-kohjani <amir.kohjani@outlook.com>
2022-11-30 14:20:37 +0330

    create wishlist page

---
+++ src/pages/WishList/WishListPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import WishList from '../../components/wishList/WishList'
+
+const WishListPage = () => {
+  return (
+    <WishList items={[]}/>
+  )
+}
+
+export default WishListPage
