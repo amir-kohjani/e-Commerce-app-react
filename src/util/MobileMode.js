be6a98ea04e91dd2d054a19a8f283c0e31d6cda1
amir-kohjani <amir.kohjani@outlook.com>
2022-12-05 23:09:45 +0330

    add userDiveces
    By using this method, you can understand what kind of device the user is.
    Mobile or desktop

--- src/util/MobileMode.js
+++ src/util/MobileMode.js
@@ -4,3 +4,16 @@
 
     return window.outerWidth < 580 ? true : false;
  }
+
+ export function userDevices(){
+   
+      const ua = navigator.userAgent;
+      if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+          return "tablet";
+      }
+      else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+          return "mobile";
+      }
+      return "desktop";
+
+ }
