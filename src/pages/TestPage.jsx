3103d6f775da59ab7797f8ed71175c2b93e7cf91
amir-kohjani <amir.kohjani@outlook.com>
2022-12-05 23:07:08 +0330

    some edit

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 import {useQuery} from 'react-query'
 import axios from 'axios'
+import { userDevices } from "../util/MobileMode";
 
 
 const ContainerMain = styled.div`
@@ -11,45 +12,20 @@ const ContainerMain = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  width: 100%;
+
   margin: 20px;
 `;
 const WithotRedux = styled.div``;
 const TestPage = () => {
 
-
+const [userDevice] = useState(userDevices) 
   return (
     <ContainerMain>
-     <Posts/>
+     <p>{userDevice}</p>
     </ContainerMain>
   );
 };
 
-function Posts(){
-  async function fetchPosts(){
-    const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts')    
-    return data
-}
-  const {data, error, isError, isLoading } = useQuery('posts', fetchPosts) 
-  // first argument is a string to cache and track the query result
-  if(isLoading){
-      return <div>Loading...</div>
-  }
-  if(isError){
-      return <div>Error! {error.message}</div>
-  }
-console.log(data)
-  return(
-      <div className='container'>
-      <h1>Posts</h1>
-      {
-          data.map((post, index) => {
-              return <li key={index}>{post.title}</li>
-          })
-      }
 
-      </div>
-  )
-}
 
 export default TestPage;
