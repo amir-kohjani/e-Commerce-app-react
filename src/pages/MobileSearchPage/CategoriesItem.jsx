59589da674041eb6f9bca38a8f0df1b0d9f52b92
amir-kohjani <amir.kohjani@outlook.com>
2022-12-11 10:16:38 +0330

    update CategoriesItem.jsx

--- src/pages/MobileSearchPage/CategoriesItem.jsx
+++ src/pages/MobileSearchPage/CategoriesItem.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container, Image, Title, TitleWrapper } from './styles/categoriesItemStyles'
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Container,
+  Image,
+  Title,
+  TitleWrapper,
+} from "./styles/categoriesItemStyles";
 
-const CategoriesItem = ({to,title,img}) => {
+const CategoriesItem = ({ to, title, img }) => {
   return (
     <Container>
-        <Link to={to}>
-            <TitleWrapper>
-                <Title>{title}</Title>
-            </TitleWrapper>
-            <Image src={img}/>
-        </Link>
+      <Link to={"/category" + to}>
+        <TitleWrapper>
+          <Title>{title}</Title>
+        </TitleWrapper>
+        <Image src={img} />
+      </Link>
     </Container>
-  )
-}
+  );
+};
 
-export default CategoriesItem
+export default CategoriesItem;
