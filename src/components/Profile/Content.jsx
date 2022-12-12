10c8b79ed77b476a9a5265750aa81515703aaed3
amir-kohjani <amir.kohjani@outlook.com>
2022-12-12 22:46:45 +0330

    Create Content.jsx

---
+++ src/components/Profile/Content.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "./styles/contentStyles";
+
+const Content = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default Content;
